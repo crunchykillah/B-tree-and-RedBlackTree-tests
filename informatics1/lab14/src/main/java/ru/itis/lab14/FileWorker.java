b395ff486f86dbe416497e84c5867035fab2c58a
ArtVolkov <mr.brick83@gmail.com>
2022-12-29 15:39:05 +0300

    Написан метод обработки строки, и метод разбиения на массив строк

---
+++ informatics1/lab14/src/main/java/ru/itis/lab14/FileWorker.java
@@ -0,0 +1,46 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+
+public class FileWorker {
+    private String str;
+    private String[] splitted;
+
+
+    public String fileReader(String fileName) {
+
+        try {
+            str = new String(Files.readString(Path.of(fileName)));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return str;
+
+    }
+    public String readEdit(){
+        if (fileReader("kniga1.txt") != null){
+            String resultString;
+            resultString = fileReader("kniga1.txt").replaceAll("\\n\\r|\\r|\\n|\\s| ", "_");
+            resultString = resultString.replaceAll("[^(A-Za-zА-Яа-я|_) ]", "");
+            resultString = resultString.replaceAll("__", "_");
+            resultString = resultString.replaceAll("__", "_");
+            resultString = resultString.toUpperCase();
+            return resultString;
+        }
+        return "строки не существует";
+    }
+
+
+    public String[] readSplit() {
+
+        splitted = readEdit().split("_");
+        Arrays.sort(splitted);
+        return splitted;
+
+    }
+
+
+
+
+}
