57ceb154176b9908eb23168b32b2df877e446278
ArtVolkov <mr.brick83@gmail.com>
2023-05-16 11:09:44 +0300

    RedBlackTree testing

---
+++ AADS/SemestrovkaRedBlackTree/src/main/java/NilNode.java
@@ -0,0 +1,7 @@
+class NilNode extends Node {
+    static final boolean BLACK = true;
+    public NilNode() {
+        super(0);
+        this.color = BLACK;
+    }
+}
---
+++ AADS/SemestrovkaRedBlackTree/src/main/java/Node.java
@@ -0,0 +1,13 @@
+public class Node {
+    int data;
+
+    Node left;
+    Node right;
+    Node parent;
+
+    boolean color;
+
+    public Node(int data) {
+        this.data = data;
+    }
+}
